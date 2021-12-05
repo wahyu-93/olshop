f059694082a4ac4cda3c2c0b7884239537102709
wahyu93 <wahyu.exam07@gmail.com>
2021-12-05 14:23:37 +0800

    eager load orders in admin

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -11,6 +11,7 @@ class OrderController extends Controller
     public function index()
     {
         $orders = Order::latest()->paginate(env('PAGINATION_PER_PAGE='));
+        $orders->load('user');  
         return view('admin.order.index', compact('orders'));
     }
 
